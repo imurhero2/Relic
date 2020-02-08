847b0beb19786bea77a97a15012f4809ece3d295
imurhero2 <35977754+imurhero2@users.noreply.github.com>
2020-02-08 15:34:00 -0500

    My Test Scene + TMP Import

---
+++ Assets/JP Testing/CoordinateText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class CoordinateText : MonoBehaviour
+{
+	public TMP_Text coordinateText;
+
+	private void Start()
+	{
+		coordinateText.text = $"({transform.position.x},{transform.position.z})";
+	}
+
+	private void Update()
+    {
+		if (transform.hasChanged)
+		{
+			transform.position = new Vector3(Mathf.Floor(transform.position.x), 0, Mathf.Floor(transform.position.z));
+			coordinateText.text = $"({transform.position.x},{transform.position.z})";
+		}
+    }
+}
---
+++ Assets/JP Testing/MapEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapEditor : EditorWindow
+{
+	public int width;
+	public int height;
+
+	[MenuItem("Window/Map Editor")]
+	public static void ShowWindow()
+	{
+		GetWindow<MapEditor>("Map Editor");
+	}
+
+	private void OnGUI()
+	{
+		width = EditorGUILayout.IntField("Width:", width);
+		height = EditorGUILayout.IntField("Height:", height);
+
+		if (GUILayout.Button("Generate Map"))
+		{
+			GenerateMap();
+		}
+	}
+
+	private void GenerateMap()
+	{
+		Debug.Log($"Generating Map of size {width}x{height}");
+	}
+}
